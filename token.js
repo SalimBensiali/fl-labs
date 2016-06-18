ff508d39581aac766784fc275cd715efb4e3b34b
xserverinternet <oliver.heilig@ptvgroup.com>
2016-06-19 01:15:14 +0200

    Token update

---
+++ token.js
@@ -0,0 +1,3 @@
+// The token below is for internal use only - consider getting your own token!
+// This token is valid until 2016-08-26T16:03:41Z and was auto-generated @ 2016-06-18T22:05:54Z.
+var token = 'AC29601B-EF3D-4B6B-AC09-9BFC8888D588';
